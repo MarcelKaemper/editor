5af5f56cb55f304c278268c75865313a30915a82
Marcel <marcelkaemper862@gmail.com>
2019-05-14 21:35:50 +0200

    Added command mode and basic CLI

--- main.c
+++ main.c
@@ -15,19 +15,36 @@ int main(int argc, char ** argv) {
     noecho();
     keypad(stdscr, TRUE);
 
-	for(int i = 0; i<h; i++){
-		for(int j = 0; j<w; j++){
-			if((i==0) || (i==h-1)){
-				mvaddch(i,j,ACS_BLOCK);
-			}else{
-				mvaddch(i, 0, ACS_BLOCK);
-				mvaddch(i, w-1, ACS_BLOCK);
+	while(1){
+		for(int i = 0; i<h; i++){
+			for(int j = 0; j<w; j++){
+				if((i==0) || (i==h-1)){
+					mvaddch(i,j,ACS_BLOCK);
+				}else{
+					mvaddch(i, 0, ACS_BLOCK);
+					mvaddch(i, w-1, ACS_BLOCK);
+				}
 			}
 		}
+		refresh();
+
+		char c;
+		
+		if((c = getch()) == 'c'){
+			for(int i = 0; i<w; i++){
+				mvaddch(h-3, i, ACS_BLOCK);
+			}
+			move(h-2, 2);
+			while((c = getch()) != 10){
+				addch(c);
+			}
+		}else if(c == 10){
+			break;	
+		}
+
+		clear();
 	}
 
-	refresh();
-	getch();
 
     endwin();
 
